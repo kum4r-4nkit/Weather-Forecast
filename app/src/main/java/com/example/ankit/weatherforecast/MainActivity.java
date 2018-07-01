713143cea625268a0ef42467da64e85b5d982cd1
Ankit <kumar805231@gmail.com>
2018-07-01 15:38:18 +0530

    Updating Data from Server.

---
+++ app/src/main/java/com/example/ankit/weatherforecast/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ankit.weatherforecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return id == R.id.action_setting||onOptionsItemSelected(item);
+    }
+}
